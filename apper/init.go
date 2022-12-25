bd6bb1b23df9375a816cb33140a5c72289320376
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-24 22:47:35 -0500

    next step is to put the ball in, and get it to react to physics, and create wrappers for adding forced and stuff like that

--- apper/init.go
+++ apper/init.go
@@ -3,13 +3,19 @@ package apper
 import (
 	"github.com/g3n/engine/camera"
 	"github.com/g3n/engine/core"
+	"github.com/g3n/engine/light"
+	"github.com/g3n/engine/math32"
 	"github.com/g3n/engine/util/helper"
 )
 
 func (a *App) Init() {
 
 	a.Cam.Orbit = camera.NewOrbitControl(a.Cam.Self)
-	a.Add2Scene(a.Cam.Self)
+	a.Add2Scene(
+		a.Cam.Self,
+		Light(100, 100, 100, "white", 1),
+		Light(100, 100, -100, "white", 1),
+	)
 
 	a.A.Gls().ClearColor(.5, .75, 2, .5)
 	if a.Help {
@@ -30,10 +36,19 @@ func (a *App) ToggleHelp(h bool) {
 	}
 }
 
-func (a *App) AddSurface() {
+func Light(
+	posX,
+	posY,
+	posZ float32,
+	color string,
+	alpha float32,
+) *light.Directional {
 
-}
+	l := light.NewDirectional(
+		math32.NewColor(color),
+		alpha,
+	)
+	l.SetPosition(posX, posY, posZ)
 
-func (a *App) AddLight() {
-
+	return l
 }
