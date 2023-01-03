ce7ba332770307b55fef84b8ef5ccaaffab6f6be
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2023-01-03 17:56:36 -0500

    added cm2ft function in conversions

--- apper/phys/conversions.go
+++ apper/phys/conversions.go
@@ -2,14 +2,15 @@ package phys
 
 const mileFt = 5280
 
-// Kg2G -> converts kilograms to grams
 func Kg2G(kg float32) float32 {
 	return kg * 1000
 }
 
-// Mph2Fps -> converts miles per hour
-// into feet per second
 func Mph2Fps(mph float32) float32 {
 	ftHr := mph * mileFt
 	return (ftHr / (60)) / 60
+}
+
+func Cm2Ft(cm float32) float32 {
+	return (cm * 0.393701) / 12
 }
